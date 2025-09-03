9fc26566e1520748a83d3b716d1d370cf9aef4d8
furqanks <furqanks@gmail.com>
2025-09-03 17:36:35 +0500

    Fix Webpack cache rename error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (!dev && (process.env.CI === 'true' || process.env.NEXT_DISABLE_WEBPACK_CACHE === '1')) {
+      config.cache = false;
+    }
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
